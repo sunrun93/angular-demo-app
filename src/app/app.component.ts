4c084e4f6398f90fe3e9bcb569eb469bc18ced0b
sunrun93 <wangjian9302@163.com>
2018-12-09 21:49:15 +0800

    update tab menu style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
-interface MenuItem{
-  label:string;
-  icon:string
-}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,20 +7,5 @@ interface MenuItem{
 })
 export class AppComponent {
   title = 'app';
-
-  items: MenuItem[];
-  activeItem: MenuItem;
-
-  ngOnInit() {
-    this.items = [
-      { label: 'Stats', icon: 'fa fa-fw fa-bar-chart' },
-      { label: 'Calendar', icon: 'fa fa-fw fa-calendar' },
-      { label: 'Documentation', icon: 'fa fa-fw fa-book' },
-      { label: 'Support', icon: 'fa fa-fw fa-support' },
-      { label: 'Social', icon: 'fa fa-fw fa-twitter' }
-    ];
-
-    this.activeItem = this.items[2];
-  }
+  
 }
-
