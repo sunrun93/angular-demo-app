de8d7c65c3fe737a2ba5ba7892c6e57eb0e6aceb
sunrun93 <wangjian9302@163.com>
2019-03-31 14:34:01 +0800

    add array utils

--- src/app/tab-view/tab-b/tab-b.component.ts
+++ src/app/tab-view/tab-b/tab-b.component.ts
@@ -10,20 +10,43 @@ import { ArrayExtention } from '../../commons/arrayExtention';
 export class TabBComponent implements OnInit {
 
   testId:string;
+
+  strArray = ['apple','watermelon','strawberry','pineapple'];
+  nbrArray = [444,222,333,111];
+  objArray = [
+    {'title':'apple','favorite':'3'},
+    {'title':'watermelon','favorite':'2'},
+    {'title':'strawberry','favorite':'1'},
+    {'title':'pineapple','favorite':'4'},
+  ];
+
+  objArrayColne:any;
+
   constructor( private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.testId = this.route.snapshot.paramMap.get('id');
   }
 
-  public static myMap = () => {
-    
+  public mySort = (array: any[], key: string = null, asce: boolean = true) => {
+    array.sort((x, y) => {
+      let targetX = key ? x[key] : x;
+      let targetY = key ? y[key] : y;
+      if (targetX < targetY) {
+        return asce ? -1 : 1;
+      } else if (targetX === targetY) {
+        return 0;
+      } else {
+        return asce ? 1 : -1;
+      }
+    })
   }
 
-  testmap(){
-    let arr = ['a','b'];
-    // arr.myMap
-  
+  public myClone = (array)=>{
+    return array.map((item)=>{return {...item}});
   }
+
+  
+
 
 }
